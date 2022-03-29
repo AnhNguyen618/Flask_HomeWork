a0795f1714c22ba18a335e8a841ae616b3ca0bd8
AnhNguyen618 <99697353+AnhNguyen618@users.noreply.github.com>
2022-03-28 20:50:07 -0700

    Add files via upload

---
+++ home.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import session
+from flask import abort
+from flask import flash
+import os
+
+
+app_obj = Flask(__name__)
+
+app_obj.config['DEBUG'] =True
+
+name = 'Lisa'
+
+city_names = ["Paris","London","Rome","Tahiti"]
+
+@app_obj.route('/')
+def home():
+    return f'''<html>
+    <head>
+        <title>Homework 3</title>
+    </head>
+    <body>
+        <h1>Welcome {name}!</h1>
+        <p>
+            <a href ="www.google.com">not google</a>
+        <ul>
+            <li>{city_names[0]}</li>
+            <li>{city_names[1]}</li>
+            <li>{city_names[2]}</li>
+            <li>{city_names[3]}</li>
+        </ul>
+        </p>
+    </body>
+</html>'''
+
+app_obj.run()
+
+
